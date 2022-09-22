54355ec4541083fffa46e30e39d2aa4fdc126c79
久治明千树汐 <admin@soraharu.com>
2022-09-22 22:09:39 +0800

    chore: remove the python runtime environment from the code repository

--- python/Lib/site-packages/wx/lib/art/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Currently provides some flag icons based on:
-http://www.famfamfam.com/lab/icons/flags
-"""
--- python/Lib/site-packages/wx/lib/plot/__main__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-if __name__ == '__main__':
-    from .examples import demo
-    demo.run_demo()
